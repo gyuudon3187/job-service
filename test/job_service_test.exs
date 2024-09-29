3171c0f03ea7f17fb9c6505ec1776397163a3323
Daniel Dahlberg <ddahlb@kth.se>
2024-09-30 00:01:58 +0200

    Set up initial testing for valid payloads to /skillset.

--- test/job_service_test.exs
+++ test/job_service_test.exs
@@ -1,8 +1,38 @@
 defmodule JobServiceTest do
   use ExUnit.Case
+  use Plug.Test
   doctest JobService
 
-  test "greets the world" do
-    assert JobService.hello() == :world
+  alias JobService.Router
+
+  @opts Router.init([])
+  @valid_skillset %{
+    "jobId" => 1,
+    "skillset" => [
+      %{
+        "topic" => "Experience in serverless architecture and AWS",
+        "importance" => 9,
+        "type" => "technical"
+      },
+      %{
+        "topic" => "Working in an agile environment with Scrum or Kanban",
+        "importance" => 7,
+        "type" => "project management"
+      }
+    ]
+  }
+
+  describe "POST /skillset" do
+    test "with valid data" do
+      # Given
+      conn = conn(:post, "/skillset", @valid_skillset)
+
+      # When
+      conn = Router.call(conn, @opts)
+
+      # Then
+      assert conn.status == 200
+      assert conn.resp_body == "Data saved successfully"
+    end
   end
 end
