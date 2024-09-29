3a460052918e0b29d4a59f6c80997d073175194f
Daniel Dahlberg <ddahlb@kth.se>
2024-09-29 23:57:19 +0200

    Added dependencies.

--- mix.exs
+++ mix.exs
@@ -22,6 +22,14 @@ defmodule JobService.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:bandit, "~> 1.5"},
+      {:plug, "~> 1.12"},
+      {:jason, "~> 1.3"},
+      {:ecto, "~> 3.10"},
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:joken, "~> 2.5"},
+      {:mox, "~> 1.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
