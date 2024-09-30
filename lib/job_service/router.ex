9e93b5d0486562d60fb02551c363f69650aef586
Daniel Dahlberg <ddahlb@kth.se>
2024-09-30 21:19:53 +0200

    Added dummy endpoints for checking application health (WIP).

--- lib/job_service/router.ex
+++ lib/job_service/router.ex
@@ -9,12 +9,26 @@ defmodule JobService.Router do
 
   post("/skillset", do: JobController.handle_skillset_request(conn))
 
-  get "/favicon.ico" do
-    # Respond with no content
-    send_resp(conn, 204, "")
+  get("/favicon.ico", do: send_resp(conn, 204, ""))
+
+  get "/alive" do
+    send_resp(conn, 200, "OK")
+  end
+
+  get "/ready" do
+    if database_ready?() do
+      send_resp(conn, 200, "OK")
+    else
+      send_resp(conn, 503, "DATABASE_UNAVAILABLE")
+    end
   end
 
   match _ do
     send_resp(conn, 404, "Not found")
+  end
+
+  @spec database_ready?() :: boolean
+  defp database_ready? do
+    true
   end
 end
